7492dfb8124c3bffd9246c5da55142782b9e5bb4
Sudo-Self <sudo-self@proton.me>
2024-01-26 11:24:50 -0700

    Update +page.svelte

--- src/routes/node/+page.svelte
+++ src/routes/node/+page.svelte
@@ -5,7 +5,7 @@
 	export let data;
 </script>
 
-<h1><span>Hello from Node!</span></h1>
+<h1><span>ip.JesseJesse.com&nbsp; Node!</span></h1>
 
 <div class="info">
 	<Details city={data.city} ip={data.ip} />
